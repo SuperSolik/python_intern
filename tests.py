91f37aa41d22e620b87a40887edb6a71c802f73b
SuperSolik <serpetr99@gmail.com>
2020-11-26 20:51:11 +0300

    task done

--- tests.py
+++ tests.py
@@ -1,14 +1,35 @@
-"""
-app tests example
-
-"""
+import aiohttp
+import pytest
 
 from app import is_alive_host
 
 
-def test_live():
-    assert is_alive_host('semrush.com')
+@pytest.fixture
+async def client_test_session():
+    session = aiohttp.ClientSession()
+    yield session
+    await session.close()
 
 
-def test_down():
-    assert not is_alive_host('invalid.domain.son')
+@pytest.mark.asyncio
+async def test_live(client_test_session):
+    is_alive = await is_alive_host('https://google.com', client_test_session)
+    assert is_alive
+
+
+@pytest.mark.asyncio
+async def test_live_redirect(client_test_session):
+    is_alive = await is_alive_host('semrush.com', client_test_session)
+    assert is_alive   
+
+
+@pytest.mark.asyncio
+async def test_down_404(client_test_session):
+    is_alive = await is_alive_host('9gag.com/404', client_test_session)
+    assert not is_alive    
+
+
+@pytest.mark.asyncio
+async def test_down_non_existing(client_test_session):
+    is_alive = await is_alive_host('invalid.test.host.com', client_test_session)
+    assert not is_alive
